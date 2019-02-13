6352755fd34cea6327a3cf7c7e23b90584c190ea
lturmel <lturmel@expertasolutions.com>
2019-02-13 12:34:37 -0500

    az network dns record-set a delete implementation
    Signed-off-by: lturmel <lturmel@expertasolutions.com>

--- aRecordManager/src/adminADnsRecord.ps1
+++ aRecordManager/src/adminADnsRecord.ps1
@@ -19,26 +19,35 @@ param (
   [string]$ipAddress
 )
 
-write-host $actionType
-
 $loginResult = az login --service-principal -u $servicePrincipalId -p $servicePrincipalKey --tenant $tenantId
 $setSubResult = az account set --subscription $subscriptionId
 
 $domainInfo = az network dns record-set a list --resource-group $resourceGroupName --zone-name $domainName --subscription $subscriptionId | ConvertFrom-Json
 $exists = $domainInfo | Where-Object { $_.name -eq $aName }
-if($exists){
-  write-host "'$($aName).$domainName' -> Current IP: '$($exists.arecords[0].ipv4Address)' vs New IP: '$($ipAddress)' ... " -NoNewline
-  
-  if($exists.arecords[0].ipv4Address -eq $ipAddress) {
-    write-host "Nothing to change"  
-  } else {
-    $result = az network dns record-set a update --resource-group $resourceGroupName --zone-name $domainName --subscription $subscriptionId --name $aName --set "arecords[0].ipv4Address=$ipAddress" --force-string | ConvertFrom-Json
-    write-host "Record updated !"
-  }
 
-} else {
-  write-host "Creating '$($aName).$domainName' ... " -NoNewline
-  $result = az network dns record-set a add-record --resource-group $resourceGroupName --zone-name $domainName --subscription $subscriptionId --record-set-name $aName --ipv4-address $ipAddress | ConvertFrom-Json
-  write-host "Record created !";
+if($actionType -eq "createUpdate") {
+  if($exists){
+    write-host "'$($aName).$domainName' -> Current IP: '$($exists.arecords[0].ipv4Address)' vs New IP: '$($ipAddress)' ... " -NoNewline
+    
+    if($exists.arecords[0].ipv4Address -eq $ipAddress) {
+      write-host "Nothing to change"  
+    } else {
+      $result = az network dns record-set a update --resource-group $resourceGroupName --zone-name $domainName --subscription $subscriptionId --name $aName --set "arecords[0].ipv4Address=$ipAddress" --force-string | ConvertFrom-Json
+      write-host "Record updated !"
+    }
+  } else {
+    write-host "Creating '$($aName).$domainName' ... " -NoNewline
+    $result = az network dns record-set a add-record --resource-group $resourceGroupName --zone-name $domainName --subscription $subscriptionId --record-set-name $aName --ipv4-address $ipAddress | ConvertFrom-Json
+    write-host "Record created !";
+  }
+} elseif($actionType -eq "remove") {
+  if($exists){
+    write-host "'$($aName).$domainName' -> Current IP: '$($exists.arecords[0].ipv4Address)' vs New IP: '$($ipAddress)' ... " -NoNewline
+    $result = az network dns record-set a delete --resource-group $resourceGroupName --zone-name $domainName --subscription $subscriptionId --name $aName --yes
+  } else {
+    write-host "'$($aName).$domainName' not existing..."
+  }
 }
+
+
 $logoutResult = az account clear
