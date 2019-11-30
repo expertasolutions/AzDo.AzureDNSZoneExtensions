59d8a9c5670a886f2edee8b0afb54ac1538fccee
lturmel <lturmel@expertasolutions.com>
2019-11-29 19:36:44 -0500

    base impl
    Signed-off-by: lturmel <lturmel@expertasolutions.com>

---
+++ aRecordManager/manageazurednszone.ts
@@ -0,0 +1,13 @@
+import tl = require('azure-pipelines-task-lib/task');
+import msRestAzure = require('ms-rest-azure');
+import dnsManagementClient = require('azure-arm-dns');
+
+async function run() {
+  try {
+    
+  } catch (err) {
+    tl.setResult(tl.TaskResult.Failed, err.message || 'run() failed');
+  }
+}
+
+await run();
