eb488d332758a6a21b8e2d70b3cb07db4bd6ddbf
lturmel <lturmel@expertasolutions.com>
2019-11-30 08:17:19 -0500

    cname base refactoring
    Signed-off-by: lturmel <lturmel@expertasolutions.com>

--- cnameRecordManager/managecnamerecord.ts
+++ cnameRecordManager/managecnamerecord.ts
@@ -1,10 +1,42 @@
-import tl = require('azure-pipelines-task-lib/task');
-import msRestAzure = require('ms-rest-azure');
-import dnsManagementClient = require('azure-arm-dns');
+import * as tl from 'azure-pipelines-task-lib/task';
+import * as msRestNodeAuth from '@azure/ms-rest-nodeauth';
+import * as dns from '@azure/arm-dns';
+
+async function LoginToAzure(servicePrincipalId:string, servicePrincipalKey:string, tenantId:string) {
+  return await msRestNodeAuth.loginWithServicePrincipalSecret(servicePrincipalId, servicePrincipalKey, tenantId );
+};
 
 async function run() {
   try {
+    let azureEndpointSubscription = tl.getInput("azureSubscriptionEndpoint", true) as string;
+    let resourceGroupName = tl.getInput("resourceGroupName", true) as string;
+    let domainName = tl.getInput("domainName", true) as string;
+    let cname = tl.getInput("cname", true) as string;
+    let alias = tl.getInput("alias", true) as string;
+    let actionType = tl.getInput("actionType", true) as string;
+    let ttl = parseInt(tl.getInput("ttl", true) as string);
     
+    let subcriptionId = tl.getEndpointDataParameter(azureEndpointSubscription, "subscriptionId", false) as string;
+
+    let servicePrincipalId = tl.getEndpointAuthorizationParameter(azureEndpointSubscription, "serviceprincipalid", false) as string;
+    let servicePrincipalKey = tl.getEndpointAuthorizationParameter(azureEndpointSubscription, "serviceprincipalkey", false) as string;
+    let tenantId = tl.getEndpointAuthorizationParameter(azureEndpointSubscription,"tenantid", false) as string;
+
+    console.log("SubscriptionId: " + subcriptionId);
+    console.log("ServicePrincipalId: " + servicePrincipalId);
+    console.log("ServicePrincipalKey: " + servicePrincipalKey);
+    console.log("TenantId: " + tenantId);
+    console.log("ResourceGroupName: " + resourceGroupName);
+    console.log("ActionType: " + actionType);
+    console.log("DomainName: " + domainName);
+    console.log("CName: " + cname);
+    console.log("Alias: " + alias);
+    console.log("TTL: " + ttl);
+    console.log("");
+
+    const azureCredentials = await LoginToAzure(servicePrincipalId, servicePrincipalKey, tenantId);
+    const dnsClient = new dns.DnsManagementClient(azureCredentials, subcriptionId);
+
   } catch (err) {
     tl.setResult(tl.TaskResult.Failed, err.message || 'run() failed');
   }
